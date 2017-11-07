6faa64ab6a53ebb2e6f4b256eb197c1cbc5e8be2
zhangxusheng <zhangxusheng@10.163.130.138>
2017-11-07 16:08:21 +0800

    update

--- main.py
+++ main.py
@@ -6,10 +6,25 @@ Created on 2017.11.3
 
 import docker
 
+VERSION = "1.30"
+IMAGE_NAME = "cherry:version3"
+COMMAND = ["cd /root/Project", "./start_celery_worker.sh"]
+CPU_PER_CON = 2
+
 if __name__ == '__main__':
     client = docker.from_env(version="1.30")
     imageList = client.images.list()
     print imageList
-    containerList = client.containers.list()
+    for i in range(3):
+        oneContainer = client.containers.run(image = IMAGE_NAME,
+                                             command = COMMAND, 
+                                             cpuset_cpus = str(i*CPU_PER_CON)+"-"+str((i+1)*CPU_PER_CON-1),
+                                             environment = ["worker_name=cherryDK_"+i],
+                                             name = "cherryDK_"+i,
+                                             mem_limit = str(2*CPU_PER_CON)+"g")
+        print oneContainer.logs()
+    
+    containerList = client.containers.list(all=True)
     print containerList
+    
     
