ccaae141175117b9b92e89e391cd04907024b2d6
zhangxusheng <zhangxusheng@192.168.3.3>
2017-12-15 02:23:03 +0800

    add input

--- noLimit.py
+++ noLimit.py
@@ -9,12 +9,13 @@ import docker
 VERSION = "1.30"
 IMAGE_NAME = "cherry:version3"
 COMMAND = ["/bin/bash", "/root/Project/start_celery_worker.sh"]
-NUM = 3
+# NUM = 3
 
 if __name__ == '__main__':
     client = docker.from_env(version="1.30")
     imageList = client.images.list()
     print imageList
+    NUM = input("Please input the container number:")
     for i in range(NUM):
         oneContainer = client.containers.run(image = IMAGE_NAME,
                                              command = COMMAND,
